e4c0106cca018358310d815244a0fb8fcb75dfb1
Chi-hung Wen <wen00033@algonquinlive.com>
2023-06-07 13:13:17 -0400

    updated

--- script.js
+++ script.js
@@ -6,13 +6,34 @@ naviagtionToggle.addEventListener("click", function () {
   console.log(visibility);
   if (visibility == "false") {
     naviagtionBar.setAttribute("data-expend", true);
-    naviagtionBar.setAttribute("aria-expend", true);
     naviagtionToggle.setAttribute("data-toggle", true);
-    naviagtionToggle.setAttribute("aria-toggle", true);
   } else {
     naviagtionBar.setAttribute("data-expend", false);
-    naviagtionBar.setAttribute("aria-expend", false);
     naviagtionToggle.setAttribute("data-toggle", false);
-    naviagtionToggle.setAttribute("aria-toggle", false);
   }
+});
+
+// -------------------------------------------------
+const tabs = document.querySelectorAll(".artwork__tab");
+const tabsContainer = document.querySelector(".artwork-tab-container");
+const tabsContent = document.querySelectorAll(".artwork__content");
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".artwork__tab");
+  console.log(clicked);
+  if (!clicked) return;
+  // active tab
+  tabs.forEach((t) => t.classList.remove("artwork__tab--active"));
+  clicked.classList.add("artwork__tab--active");
+  tabsContent.forEach((c) => c.classList.remove("artwork__content--active"));
+  tabsContent.forEach((c) => c.classList.add("artwork__content"));
+
+  // console.log(clicked.dataset.tab);
+  // activitated the area
+  document
+    .querySelector(`.artwork__content--${clicked.dataset.tab}`)
+    .classList.add("artwork__content--active");
+  document
+    .querySelector(`.artwork__content--${clicked.dataset.tab}`)
+    .classList.remove("artwork__content");
 });
